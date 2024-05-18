a39a1db84c2b743d497bc51a9330fb7f42e6b1e5
包子 <chengbaozi6@qq.com>
2024-05-18 18:48:17 +0800

    <feature>add:第二种存档方式

--- Assets/Scripts/GameBussiness.cs
+++ Assets/Scripts/GameBussiness.cs
@@ -15,9 +15,11 @@ public static class GameBussiness {
 
         Vector2 pos = new Vector2(0, 0);
 
-        pos = LoadType1();
-        
+        // pos = LoadType1();
+        pos = LoadType2();
+
         ctx.roleEntity.SetPos(pos);
+
 
     }
 
@@ -27,9 +29,10 @@ public static class GameBussiness {
         Vector2 pos = ctx.roleEntity.transform.position;
 
         // 存到本地
-        SaveType1(pos);
+        // SaveType1(pos);
+        SaveType2(pos);
     }
-
+    #region SaveType1
     static void SaveType1(Vector2 pos) {
         string x = pos.x.ToString();
         string y = pos.y.ToString();
@@ -44,4 +47,40 @@ public static class GameBussiness {
         float posY = float.Parse(y);
         return new Vector2(posX, posY);
     }
+    #endregion
+
+    #region SaveType2
+    static void SaveType2(Vector2 pos) {
+
+        byte[] data = new byte[8];
+
+        float x = pos.x;
+        int xInt = (int)(pos.x);
+        int yInt = (int)(pos.y);
+
+        data[0] = (byte)(xInt);
+        data[1] = (byte)(xInt >> 8);
+        data[2] = (byte)(xInt >> 16);
+        data[3] = (byte)(xInt >> 24);
+
+        data[4] = (byte)(yInt);
+        data[5] = (byte)(yInt >> 8);
+        data[6] = (byte)(yInt >> 16);
+        data[7] = (byte)(yInt >> 24);
+
+        File.WriteAllBytes("/save.dat", data);
+
+    }
+
+    static Vector2 LoadType2() {
+        byte[] data = File.ReadAllBytes("/save.dat");
+
+        int xInt = data[0] | data[1] << 8 | data[2] << 16 | data[3] << 24;
+        int yInt = data[4] | data[5] << 8 | data[6] << 16 | data[7] << 24;
+
+        return new Vector2(xInt, yInt);
+
+    }
+
+    #endregion
 }
